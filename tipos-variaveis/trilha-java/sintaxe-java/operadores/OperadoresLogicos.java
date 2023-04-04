6662c55e456bc833780cf6b6cc77c0de1db14a06
Nullwell47 <emanuelr.barros16@gmail.com>
2023-04-03 22:37:31 -0300

    incluindo exemplos de operadores logicos

---
+++ tipos-variaveis/trilha-java/sintaxe-java/operadores/OperadoresLogicos.java
@@ -0,0 +1,26 @@
+public class OperadoresLogicos {
+
+    public static void main(String[] args) {
+
+    // Operadores.java
+    boolean condicao1=true;
+
+    boolean condicao2=true;
+
+    /* Aqui estamos utilizando o operador lógico E para fazer a união de duas 
+    expressões. 
+    É como se estivesse escrito:
+    "Se Condicao1 e Condicao2 forem verdadeiras, executar código"
+    */
+
+    if(condicao1 && (7 > 4) ){
+        System.out.print("as duas condições são verdadeiras" + ", ");;
+    }
+    
+    //Se condicao1 OU condicao2 for verdadeira, executar código.
+    if(condicao1 || condicao2){
+        System.out.print("uma das condições é verdadeira");
+    }        
+        }
+    
+}
