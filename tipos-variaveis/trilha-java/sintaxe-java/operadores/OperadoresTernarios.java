23313bbc038806d30e5657a4650373ee3d18e6e5
Nullwell47 <emanuelr.barros16@gmail.com>
2023-04-03 22:24:17 -0300

    incluindo exemplos de operadores ternarios

---
+++ tipos-variaveis/trilha-java/sintaxe-java/operadores/OperadoresTernarios.java
@@ -0,0 +1,25 @@
+public class OperadoresTernarios {
+    
+    public static void main(String[] args) {
+
+        int a, b;
+
+        a = 6;
+        b = 6;
+
+        String resultado = a==b ?"verdadeiro" : "falso";
+
+        int resultado2 = a==b ? 1: 0;
+
+        //String resultado = "";
+
+       /*  if(a==b)
+        resultado = "verdadeiro";
+        else
+        resultado = "falso";
+        */
+
+        System.out.println(resultado + " " + resultado2);
+        
+    }
+}
